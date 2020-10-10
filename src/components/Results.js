f33142c9b0c0280d170ba9f46a09a82ef191c529
Marco Arboleda <arboleda.marco@gmail.com>
2020-10-10 15:04:41 -0400

    Set up components

---
+++ src/components/Results.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const results = () => {
+
+    return (
+        <div className="results">
+
+        </div>
+    );
+}
+
+export default results;
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const search = () => {
+
+    return (
+        <div className="search">
+        </div>
+    );
+}
+
+export default search;
