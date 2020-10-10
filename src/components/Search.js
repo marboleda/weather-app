863cae6982526a86f4d25544db5f2a459288cf91
Marco Arboleda <arboleda.marco@gmail.com>
2020-10-10 18:24:37 -0400

    Successfully make API call

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 const search = () => {
 
+    const handleSearch = () => {
+        const cityName = document.getElementById('searchInput').value;
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a7be9f905cd3da9bb04819dd3df2b03e`, {mode: 'cors'})
+            .then((response) => {
+                console.log('Response: ', response);
+                return response.json();
+            }).then((data) => {
+                console.log(data);
+            })
+    }
+
     return (
         <div className="search">
-            <input type="text" id="searchInput" />
-            <button id="searchButton">Search</button>
+            <input type="text" id="searchInput" placeholder="Enter City Here" />
+            <button id="searchButton" onClick={handleSearch}>Search</button>
         </div>
     );
 }
