59b49c0466d66623779a874d012769086deaef07
Marco Arboleda <arboleda.marco@gmail.com>
2020-10-16 21:44:35 -0400

    Prevent any HTML from showing if weather object is empty

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,15 +3,18 @@ import React from 'react';
 const results = (props) => {
 
     const { weatherObj } = props;
+    //Check if weather object is empty and render conditionally based on that
+    //May be more useful to use styling instead to accomplish this?
+    const isWeatherObjEmpty = Object.keys(weatherObj).length === 0 && weatherObj.constructor === Object;
 
 
     return (
         <div className="results">
             <h2>{weatherObj.cityName}</h2>
             <div>
-                <span>{weatherObj.temp}</span><span>{'\u00B0'}{weatherObj.unit}</span>
+                <span>{weatherObj.temp}</span> { isWeatherObjEmpty ? null : <span>{'\u00B0'}{weatherObj.unit}</span> }
             </div>
-            <p>Feels like: <span>{weatherObj.feelsLike}</span></p>
+            { isWeatherObjEmpty ? null : <p>Feels like: <span>{weatherObj.feelsLike}</span></p> }
             <span>{weatherObj.description}</span>
         </div>
     );
