87d3fd53119547d4f65104e775a55ab3ac2817a2
Marco Arboleda <arboleda.marco@gmail.com>
2021-01-10 13:20:36 -0500

    Updates

---
+++ precache-manifest.b575d9ff037ddd09bc38690310d278d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230e35d611c94cecbdc3fc8c8c8e93fb",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "4754a875f4c7c3677028",
+    "url": "/weather-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "afafe2d1a07ec71a2c7e",
+    "url": "/weather-app/static/js/2.105bdda1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.105bdda1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4754a875f4c7c3677028",
+    "url": "/weather-app/static/js/main.01dfbab9.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
