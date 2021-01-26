ef48cc0194244aacfafd5acf8ebfbce206cd3ca8
Marco Arboleda <arboleda.marco@gmail.com>
2021-01-25 22:10:01 -0500

    Move styled component declarations out of render method for Results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Results = styled.div`
+    padding-top: 2vh;
+    background-color: #545b72;
+    flex-grow: 1;
+    text-align: center;
+`
+
+const ResultsBox = styled.div`
+    display: ${props => props.isWeatherObjEmpty ? "none" : "inline-block"};
+    padding: 10px;
+    border: 5px solid #000080;
+    border-radius: 5px;
+    background-color: #ffcc00;
+`
+
+const City = styled.h2`
+    margin-top: 0px;
+`
+
+const Unit = styled.span`
+    display: ${props => props.isWeatherObjEmpty ? "none" : "inline"};
+`
+
+const FeelsLikeSection = styled.p`
+    display: ${props => props.isWeatherObjEmpty ? "none" : "block"};
+`
+
+const UnitSelection = styled.div`
+    display: ${props => props.isWeatherObjEmpty ? "none" : "block"};
+    margin-top: 2vh;
+`
+
 const results = (props) => {
 
     const { changeUnit, unit, weatherObj } = props;
@@ -8,49 +40,17 @@ const results = (props) => {
     //Check if weather object is empty and render conditionally based on that
     const isWeatherObjEmpty = Object.keys(weatherObj).length === 0 && weatherObj.constructor === Object;
 
-    const Results = styled.div`
-        padding-top: 2vh;
-        background-color: #545b72;
-        flex-grow: 1;
-        text-align: center;
-    `
-
-    const ResultsBox = styled.div`
-        display: ${isWeatherObjEmpty ? "none" : "inline-block"};
-        padding: 10px;
-        border: 5px solid #000080;
-        border-radius: 5px;
-        background-color: #ffcc00;
-    `
-
-    const City = styled.h2`
-        margin-top: 0px;
-    `
-
-    const Unit = styled.span`
-        display: ${isWeatherObjEmpty ? "none" : "inline"};
-    `
-
-    const FeelsLikeSection = styled.p`
-        display: ${isWeatherObjEmpty ? "none" : "block"};
-    `
-
-    const UnitSelection = styled.div`
-        display: ${isWeatherObjEmpty ? "none" : "block"};
-        margin-top: 2vh;
-    `
-
     return (
         <Results>
-            <ResultsBox>
+            <ResultsBox isWeatherObjEmpty={isWeatherObjEmpty} >
                 <City>{weatherObj.cityName}</City>
                 <div>
-                    <span>{weatherObj.temp}</span> <Unit>{'\u00B0'}{weatherObj.unit}</Unit>
+                    <span>{weatherObj.temp}</span> <Unit isWeatherObjEmpty={isWeatherObjEmpty}>{'\u00B0'}{weatherObj.unit}</Unit>
                 </div>
-                <FeelsLikeSection>Feels like: <span>{weatherObj.feelsLike}</span></FeelsLikeSection>
+                <FeelsLikeSection isWeatherObjEmpty={isWeatherObjEmpty}>Feels like: <span>{weatherObj.feelsLike}</span></FeelsLikeSection>
                 <span>{weatherObj.description}</span>
                 <br />
-                <UnitSelection>
+                <UnitSelection isWeatherObjEmpty={isWeatherObjEmpty}>
                     <input type="radio" 
                         id="celsius" 
                         name="unit" 
